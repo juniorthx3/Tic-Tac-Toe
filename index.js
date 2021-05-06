0cf2d992712f63b37adeab07b661cf7585ce8573
unknown <juniorthx@gmail.com>
2021-05-06 13:54:38 -0700

    update index.js

--- index.js
+++ index.js
@@ -6,17 +6,39 @@ let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const corners=document.querySelectorAll('.corner');
 const playGame = () => {
     document.querySelector(".results").style.board="none";
+    const winner=(board, player)=>{
+      let answer=board.reduce((x, y, z)=>(y === player) ? x.concat(z) : x, []);
+      let winner=null;
+      for(let [index, win] of scoreWinner.entries()){
+          if(win.every(elem=>answer.indexOf(elem) > -1)){
+            winner={index:index, player:player};
+            break; 
+          }
+      }
+      return winner;
+    }
+    const gameOver = (champion)=>{
+      for(let i of scoreWinner[champion, i]){
+          document.getElementById(i).style.backgroundColor=champion.player == currentPlayer ? "blue" : "red";
+      }
+     corners.forEach(corner => {
+         corner.removeEventListener('click', clickPlay, false)
+     })
+    }
+
     const clickPlay=(cross)=>{
         const play=(crossId, player)=>{
             board[crossId] = player;
             document.getElementById(crossId).innerText=player;
+            let champion=winner(board, player);
+            if (champion) gameOver(champion)
         }
         play(cross.target.id, currentPlayer)
     }
     corners.forEach(corner => {
         corner.innerHTML='';
         corner.addEventListener('click', clickPlay, false);
-    }) 
+    })  
 }
 
 playGame();
