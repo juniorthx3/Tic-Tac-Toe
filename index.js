8092234b5b8e3229cdc88fb7ddc52eaee16e47c1
unknown <juniorthx@gmail.com>
2021-05-11 08:18:20 -0700

    last updates

--- index.js
+++ index.js
@@ -1,53 +1,58 @@
 const currentPlayer='X';
 const computerPlayer='0';
 const scoreWinner=[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8],[6, 4, 2]];
-let board;
 const corners=document.querySelectorAll('.corner');
 
 const playGame = () => {
-    document.querySelector(".results").style.board="none";
-    board=Array.from(Array(9).keys());
+    document.querySelector(".results").style.display="none";
+    let board=Array.from(Array(9).keys());
+
     //Winner
     const winner=(board, player)=>{
       let answer=board.reduce((x, y, z)=>(y === player) ? x.concat(z) : x, []);
       let winner=null;
       for(let [index, win] of scoreWinner.entries() ){
-          if(win.every(elem=>answer.indexOf(elem) > -1)){
+          if(win.every(elem=>answer.indexOf(elem) > -1)){ 
             winner={index:index, player:player};
             break; 
           }
       }
       return winner;
     } 
+
     //End Game
     const gameOver = (champion)=>{
       for(let index of scoreWinner[champion.index]){ 
-          document.getElementById(index).style.backgroundColor=champion.player == currentPlayer ? "rgba(82,82,82,0.3)" : "red";
+          document.getElementById(index).style.backgroundColor=champion.player == currentPlayer ? "rgba(82,82,82,0.3)" : "rgba(255,87,51,0.3";
       }
      corners.forEach(corner => {
          corner.removeEventListener('click', clickPlay, false);
      })
       declareWinner(champion.player == currentPlayer ? "You win!" : "You lose.")
     }
+
     const emptyCorner=()=>{
         return board.filter(s => typeof s == 'number')
     }
+
     const computerPlay=()=>{
-       return emptyCorner()[0];
-    }
+        return emptyCorner()[0];
+    } 
+
     const declareWinner=(winner)=>{
-        document.querySelector(".results").style.board="block";
-        document.querySelector(".results .display").style.board=winner;
+        document.querySelector(".results").style.display="block";
+        document.querySelector(".results .display").innerText=winner;
     }
+
     const humanPlay=()=>{
         if(emptyCorner().length == 0){
             for(let i=0; i < corners.length; i++){
-                corners[i].style.backgroundColor="green";
+                corners[i].style.backgroundColor="#EE82EE";
                 corners[i].removeEventListener('click', clickPlay, false);
             }
-            declareWinner("Same level!");
+            declareWinner("Tie Game");
             return true;
-        }
+        } 
         return false;
     }
 
@@ -63,7 +68,6 @@ const playGame = () => {
             play(cross.target.id, currentPlayer);
             if(!humanPlay()) play(computerPlay(), computerPlayer);
         }
-       
     }
   
     corners.forEach(corner => {
